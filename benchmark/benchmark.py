0cc192e495bad807edefc51d6f43eac835239c72
Devan Kuleindiren <d.kuleindiren@gmail.com>
2016-11-02 19:22:28 +0000

    Prevent overflow during perplexity calculation by monitoring the size of the prob product.

--- benchmark/benchmark.py
+++ benchmark/benchmark.py
@@ -1,5 +1,5 @@
-import io
 import lm.model
+import math
 
 
 class Benchmark:
@@ -19,22 +19,40 @@ class Benchmark:
             A dictionary of evaluation metrics, containing the perplexity of the language model.
         """
         word_seq = []
-        perplexity = 1.0
+        product = 1.0
+        products = []
         num_words = 0
+        num_lines = 0
 
         with open(input_file) as f:
+
             for line in f:
                 words = line.split()
                 for word in words:
-                    num_words += 1
-                    perplexity *= (1 / self._model.prob(word, word_seq))
+                    fraction = 1 / self._model.prob(word, word_seq)
+                    new_product = product * fraction
+                    if math.isinf(new_product):
+                        products.append(product)
+                        product = fraction
+                    else:
+                        product = new_product
 
                     if word == ".":
                         word_seq = []
                     else:
                         word_seq.append(word)
+                    num_words += 1
 
-        return {"perplexity": perplexity ** (1 / num_words)}
+                num_lines += 1
+                if num_lines % 10 == 0:
+                    print("Evaluated %d lines." % num_lines)
+
+        products.append(product)
+        perplexity = 1.0
+        for product in products:
+            perplexity *= math.pow(product, 1 / num_words)
+
+        return {"perplexity": perplexity}
 
     def timing(self):
         return 0.0
