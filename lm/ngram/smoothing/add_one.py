2a6b0e69ccf00e1ca46af58aa8d9009910930039
Devan Kuleindiren <d.kuleindiren@gmail.com>
2016-11-16 20:24:33 +0000

    Remove old Python code.

--- lm/ngram/smoothing/add_one.py
+++
@@ -1,9 +0,0 @@
-import lm.ngram.model as ngram
-
-
-class AddOne(ngram.NGramLM):
-
-    def prob(self, word, word_seq):
-        trimmed_word_seq = word_seq[-self._n+1:]
-        return (self._trie.count(trimmed_word_seq + [word]) + 1) / (self._trie.sum_following(trimmed_word_seq) +
-                                                                    self._trie.vocab_size())
