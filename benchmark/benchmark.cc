0bd805bdf0d8d9d9f873972de3dfa0bd027ad674
Devan Kuleindiren <d.kuleindiren@gmail.com>
2017-01-03 15:12:44 +0000

    Make use of minimum ContextSize() to decide how many <s> markers to insert, and skip 0 probs.

--- benchmark/benchmark.cc
+++ benchmark/benchmark.cc
@@ -8,12 +8,15 @@ double Benchmark::Perplexity(std::string file_name) {
         double product = 1;
         double num_words = 0;
         std::list<double> products;
+        bool did_skip_zero_prob = false;
 
         while (std::getline(f, line)) {
             size_t pos = 0;
             std::string word;
             std::list<std::string> seq;
-            seq.push_back("<s>");
+            for (int i = 0; i < (language_model->ContextSize()).first; i++) {
+                seq.push_back("<s>");
+            }
 
             while (!line.empty()) {
                 pos = line.find(" ");
@@ -24,10 +27,11 @@ double Benchmark::Perplexity(std::string file_name) {
                 if (pos > 0) {
                     std::string word = line.substr(0, pos);
                     seq.push_back(word);
-                    num_words++;
 
-                    if (language_model->ContainsWord(word)) {
-                        double fraction = 1 / language_model->Prob(seq);
+                    double prob = language_model->Prob(seq);
+                    if (prob > 0) {
+                        num_words++;
+                        double fraction = 1 / prob;
                         double new_product = product * fraction;
                         if (isinf(new_product)) {
                             products.push_back(product);
@@ -35,6 +39,8 @@ double Benchmark::Perplexity(std::string file_name) {
                         } else {
                             product = new_product;
                         }
+                    } else {
+                        did_skip_zero_prob = true;
                     }
                 }
                 line.erase(0, pos + 1);
@@ -46,6 +52,11 @@ double Benchmark::Perplexity(std::string file_name) {
         for (std::list<double>::iterator it = products.begin(); it != products.end(); ++it) {
             perplexity *= pow(*it, 1 / num_words);
         }
+
+        if (did_skip_zero_prob) {
+            std::cout << "(skipped at least one P=0) ";
+        }
+
         return perplexity;
     }
     return 0;
@@ -59,12 +70,15 @@ double Benchmark::PerplexityExp(std::string file_name) {
         double sum = 0;
         double num_words = 0;
         std::list<double> sums;
+        bool did_skip_zero_prob = false;
 
         while (std::getline(f, line)) {
             size_t pos = 0;
             std::string word;
             std::list<std::string> seq;
-            seq.push_back("<s>");
+            for (int i = 0; i < (language_model->ContextSize()).first; i++) {
+                seq.push_back("<s>");
+            }
 
             while (!line.empty()) {
                 pos = line.find(" ");
@@ -75,10 +89,11 @@ double Benchmark::PerplexityExp(std::string file_name) {
                 if (pos > 0) {
                     std::string word = line.substr(0, pos);
                     seq.push_back(word);
-                    num_words++;
 
-                    if (language_model->ContainsWord(word)) {
-                        double logp = log(language_model->Prob(seq));
+                    double prob = language_model->Prob(seq);
+                    if (prob > 0) {
+                        num_words++;
+                        double logp = log(prob);
                         double new_sum = sum + logp;
                         if (isinf(new_sum)) {
                             sums.push_back(sum);
@@ -86,6 +101,8 @@ double Benchmark::PerplexityExp(std::string file_name) {
                         } else {
                             sum = new_sum;
                         }
+                    } else {
+                        did_skip_zero_prob = true;
                     }
                 }
                 line.erase(0, pos + 1);
@@ -97,6 +114,11 @@ double Benchmark::PerplexityExp(std::string file_name) {
         for (std::list<double>::iterator it = sums.begin(); it != sums.end(); ++it) {
             perplexity *= exp((-1.0 / num_words) * *it);
         }
+
+        if (did_skip_zero_prob) {
+            std::cout << "(skipped at least one P=0) ";
+        }
+
         return perplexity;
     }
     return 0;
