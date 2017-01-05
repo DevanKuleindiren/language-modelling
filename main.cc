bcf67bace46764f8ced8de11f8bb236eeb5ffa47
Devan Kuleindiren <d.kuleindiren@gmail.com>
2017-01-05 15:58:21 +0000

    Ignore <unk> in generation and add option to finish the sentence.

--- main.cc
+++ main.cc
@@ -10,11 +10,31 @@
 
 void usage(char* const argv_0) {
     std::cerr << "Usage: " << argv_0;
-    std::cerr << " --model_path=PATH --type=TYPE --generate=GENR" << std::endl;
+    std::cerr << " --model_path=PATH --type=TYPE --generate=GENR --finish_the_sentence=FNSH" << std::endl;
     std::cerr << "Where:" << std::endl;
     std::cerr << "    PATH is the path the directory containing the model protos." << std::endl;
     std::cerr << "    TYPE is the type of language model (one of: " << RNN << " or " << NGRAM << ")." << std::endl;
     std::cerr << "    GENR is the number of words to generate using the language model." << std::endl;
+    std::cerr << "    FNSH is set if you want the LM to finish your sentence." << std::endl;
+}
+
+std::list<std::pair<std::string, double>> GetNextK(LM *lm, std::list<std::string> seq, int k) {
+    std::list<std::pair<std::string, double>> result;
+    for (int i = 0; i < k; i++) {
+        std::list<std::pair<std::string, double>> probs;
+        lm->ProbAllFollowing(seq, probs);
+        double max_prob = 0;
+        std::string max_prediction;
+        for (std::list<std::pair<std::string, double>>::iterator it = probs.begin(); it != probs.end(); ++it) {
+            if (it->second > max_prob && (it->first).compare("<unk>") != 0) {
+                max_prob = it->second;
+                max_prediction = it->first;
+            }
+        }
+        seq.push_back(max_prediction);
+        result.push_back(std::make_pair(max_prediction, max_prob));
+    }
+    return result;
 }
 
 int main(int argc, char* argv[]) {
@@ -23,11 +43,13 @@ int main(int argc, char* argv[]) {
     std::string model_path;
     std::string type;
     int generate = 0;
+    bool finish_the_sentence = false;
 
     const bool parse_result = tensorflow::ParseFlags(&argc, argv, {
         tensorflow::Flag("model_path", &model_path),
         tensorflow::Flag("type", &type),
         tensorflow::Flag("generate", &generate),
+        tensorflow::Flag("finish_the_sentence", &finish_the_sentence),
     });
     if (!parse_result) {
         usage(argv[0]);
@@ -55,10 +77,9 @@ int main(int argc, char* argv[]) {
     if (generate > 0) {
         std::list<std::string> seq;
         seq.push_back("<s>");
-        for (int i = 0; i < generate; i++) {
-            std::pair<std::string, double> prediction;
-            lm->Predict(seq, prediction);
-            seq.push_back(prediction.first);
+        std::list<std::pair<std::string, double>> rest = GetNextK(lm, seq, generate);
+        for (std::list<std::pair<std::string, double>>::iterator it = rest.begin(); it != rest.end(); ++it) {
+            seq.push_back(it->first);
         }
         seq.pop_front();
 
@@ -85,9 +106,21 @@ int main(int argc, char* argv[]) {
             context.erase(0, pos + 1);
         }
 
-        std::pair<std::string, double> prediction;
-        lm->Predict(seq, prediction);
-
-        std::cout << "Prediction: " << prediction.first << " (" << prediction.second << ")" << std::endl;
+        std::cout << "Prediction: ";
+        std::list<std::pair<std::string, double>> next = GetNextK(lm, seq, 1);
+        if (finish_the_sentence) {
+            for (std::list<std::string>::iterator it = seq.begin(); it != seq.end(); ++it) {
+                std::cout << *it << " ";
+            }
+            while (next.front().first.compare("<s>") != 0) {
+                seq.push_back(next.front().first);
+                std::cout << next.front().first << " ";
+                next = GetNextK(lm, seq, 1);
+            }
+            std::cout << "." << std::endl;
+        } else {
+            std::cout << next.front().first << " (" << next.front().second  << ")"<< std::endl;
+        }
+        std::cout << std::endl;
     }
 }
