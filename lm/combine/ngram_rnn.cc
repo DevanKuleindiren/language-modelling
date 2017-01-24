2a2ba44b70a1051228ef18fd7ebf484d1efe56ae
Devan Kuleindiren <d.kuleindiren@gmail.com>
2017-01-24 22:57:05 +0000

    Added support for combining N-gram and RNN models.

---
+++ lm/combine/ngram_rnn.cc
@@ -0,0 +1,53 @@
+#include "ngram_rnn.h"
+
+
+bool NGramRNN::ContainsWord(std::string word) {
+    return ngram_lm->ContainsWord(word);
+}
+
+std::pair<int, int> NGramRNN::ContextSize() {
+    return std::make_pair(1, 2);
+}
+
+double NGramRNN::Prob(std::list<std::string> seq) {
+    return Prob(seq, true);
+}
+
+double NGramRNN::Prob(std::list<std::string> seq, bool use_prev_state) {
+    if (use_prev_state) {
+        prev_words.splice(prev_words.end(), seq);
+        while (prev_words.size() > ngram_lm->ContextSize().second) {
+            prev_words.pop_front();
+        }
+    } else {
+        prev_words = seq;
+    }
+    return CombineFunction(ngram_lm->Prob(prev_words), rnn_lm->Prob(seq, use_prev_state));
+}
+
+void NGramRNN::ProbAllFollowing (std::list<std::string> seq, std::list<std::pair<std::string, double>> &probs) {
+    ProbAllFollowing(seq, probs, true);
+}
+
+void NGramRNN::ProbAllFollowing (std::list<std::string> seq, std::list<std::pair<std::string, double>> &probs, bool use_prev_state) {
+    if (use_prev_state) {
+        prev_words.splice(prev_words.end(), seq);
+        while (prev_words.size() > ngram_lm->ContextSize().second) {
+            prev_words.pop_front();
+        }
+    } else {
+        prev_words = seq;
+    }
+
+    std::list<std::pair<std::string, double>> ngram_probs;
+    std::list<std::pair<std::string, double>> rnn_probs;
+
+    ngram_lm->ProbAllFollowing(prev_words, ngram_probs);
+    rnn_lm->ProbAllFollowing(seq, rnn_probs, use_prev_state);
+
+    std::list<std::pair<std::string, double>>::iterator it_ngram = ngram_probs.begin();
+    std::list<std::pair<std::string, double>>::iterator it_rnn = rnn_probs.begin();
+    for (; it_ngram != ngram_probs.end() && it_rnn != rnn_probs.end(); ++it_ngram, ++it_rnn) {
+        probs.push_back(std::make_pair(it_ngram->first, CombineFunction(it_ngram->second, it_rnn->second)));
+    }
+}
---
+++ lm/combine/ngram_rnn.h
@@ -0,0 +1,53 @@
+#ifndef ngram_rnn_h
+#define ngram_rnn_h
+
+#include "tensorflow/Source/lm/lm.h"
+#include "tensorflow/Source/lm/ngram/ngram.h"
+#include "tensorflow/Source/lm/rnn/rnn.h"
+
+
+class NGramRNN : public LM {
+protected:
+    NGram *ngram_lm;
+    RNN *rnn_lm;
+    std::list<std::string> prev_words;
+    virtual double CombineFunction(double, double) = 0;
+public:
+    NGramRNN(NGram *ngram_lm, RNN *rnn_lm) : LM(), ngram_lm(ngram_lm), rnn_lm(rnn_lm) {}
+    virtual bool ContainsWord(std::string);
+    virtual std::pair<int, int> ContextSize();
+    virtual double Prob(std::list<std::string>);
+    virtual double Prob(std::list<std::string>, bool);
+    virtual void ProbAllFollowing (std::list<std::string>, std::list<std::pair<std::string, double>> &);
+    virtual void ProbAllFollowing (std::list<std::string>, std::list<std::pair<std::string, double>> &, bool);
+};
+
+class NGramRNNAverage : public NGramRNN {
+protected:
+    double CombineFunction (double a, double b) {
+        return (a + b) / 2.0;
+    }
+public:
+    NGramRNNAverage(NGram *ngram_lm, RNN *rnn_lm) : NGramRNN(ngram_lm, rnn_lm) {}
+};
+
+class NGramRNNMax : public NGramRNN {
+protected:
+    double CombineFunction (double a, double b) {
+        return std::max(a, b);
+    }
+public:
+    NGramRNNMax(NGram *ngram_lm, RNN *rnn_lm) : NGramRNN(ngram_lm, rnn_lm) {}
+};
+
+class NGramRNNInterp : public NGramRNN {
+protected:
+    double lambda;
+    double CombineFunction (double a, double b) {
+        return (lambda * a) + ((1 - lambda) * b);
+    }
+public:
+    NGramRNNInterp(NGram *ngram_lm, RNN *rnn_lm, double lambda) : NGramRNN(ngram_lm, rnn_lm), lambda(lambda) {}
+};
+
+#endif // ngram_rnn.h
