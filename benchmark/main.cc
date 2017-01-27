0b7f8a377d4826096ad4087ee0bd83aa50b4adcc
Devan Kuleindiren <d.kuleindiren@gmail.com>
2017-01-27 16:54:03 +0000

    Add command line flags for benchmarking combined models.

--- benchmark/main.cc
+++ benchmark/main.cc
@@ -5,47 +5,62 @@
 #include "tensorflow/core/util/command_line_flags.h"
 #include "tensorflow/Source/benchmark/benchmark.h"
 #include "tensorflow/Source/benchmark/benchmark.pb.h"
+#include "tensorflow/Source/lm/combine/ngram_rnn.h"
 #include "tensorflow/Source/lm/ngram/load.h"
 #include "tensorflow/Source/lm/rnn/rnn.h"
 
-#define RNN_TYPE std::string("rnn")
-#define NGRAM_TYPE std::string("ngram")
+#define COMB_AVR std::string("avr")
+#define COMB_MAX std::string("max")
 
 void usage(char* const argv_0) {
     std::cerr << "Usage: " << argv_0;
-    std::cerr << " --model_path=PATH --type=TYPE --test_data_path=TEST" << std::endl;
+    std::cerr << " --ngram_path=PATH --rnn_path=PATH --test_data_path=TEST --save_path=SAVE --comb_type=TYPE" << std::endl;
     std::cerr << "Where:" << std::endl;
     std::cerr << "    PATH is the path the directory containing the model protos." << std::endl;
-    std::cerr << "    TYPE is the type of language model (one of: " << RNN_TYPE << " or " << NGRAM_TYPE << ")." << std::endl;
     std::cerr << "    TEST is the file path of the test data to run the benchmarking against." << std::endl;
+    std::cerr << "    SAVE is the path the directory to save the benchmark proto." << std::endl;
+    std::cerr << "    TYPE is the type of combination used for the language models (one of: ";
+    std::cerr << COMB_AVR << " or " << COMB_MAX << ")." << std::endl;
+    std::cerr << "(Note: --save_path and --comb_type are only required if both --ngram_path and --rnn_path are set.)" << std::endl;
 }
 
 int main(int argc, char* argv[]) {
     tensorflow::port::InitMain(argv[0], &argc, &argv);
 
-    std::string model_path;
-    std::string type;
+    std::string ngram_path;
+    std::string rnn_path;
     std::string test_data_path;
+    std::string save_path;
+    std::string comb_type;
 
     const bool parse_result = tensorflow::ParseFlags(&argc, argv, {
-        tensorflow::Flag("model_path", &model_path),
-        tensorflow::Flag("type", &type),
+        tensorflow::Flag("ngram_path", &ngram_path),
+        tensorflow::Flag("rnn_path", &rnn_path),
         tensorflow::Flag("test_data_path", &test_data_path),
+        tensorflow::Flag("save_path", &save_path),
+        tensorflow::Flag("comb_type", &comb_type),
     });
     if (!parse_result) {
         usage(argv[0]);
         return -1;
     }
 
-    if (model_path.empty()) {
-        std::cerr << "Error: --model_path must be set." << std::endl;
+    if (ngram_path.empty() && rnn_path.empty()) {
+        std::cerr << "Error: at least one of --rnn_path or --ngram_path must be set." << std::endl;
         usage(argv[0]);
         return -1;
     }
-    if (type.empty()) {
-        std::cerr << "Error: --type must be set." << std::endl;
-        usage(argv[0]);
-        return -1;
+    if (!ngram_path.empty() && !rnn_path.empty()) {
+        if (save_path.empty()) {
+            std::cerr << "Error: --save_path must be set if both --ngram_path and --rnn_path are set." << std::endl;
+            usage(argv[0]);
+            return -1;
+        }
+        if (comb_type.empty()) {
+            std::cerr << "Error: --comb_type must be set if both --ngram_path and --rnn_path are set." << std::endl;
+            usage(argv[0]);
+            return -1;
+        }
     }
     if (test_data_path.empty()) {
         std::cerr << "Error: --test_data_path must be set." << std::endl;
@@ -54,13 +69,31 @@ int main(int argc, char* argv[]) {
     }
 
     LM *lm;
-    if (type.compare(RNN_TYPE) == 0) {
-        lm = new RNN(model_path);
-    } else if (type.compare(NGRAM_TYPE) == 0) {
-        lm = Load(model_path);
+    NGram *ngram_lm;
+    RNN *rnn_lm;
+    if (!ngram_path.empty()) {
+        ngram_lm = Load(ngram_path);
+    }
+    if (!rnn_path.empty()) {
+        rnn_lm = new RNN(rnn_path);
+    }
+
+    if (!ngram_path.empty()) {
+        if (!rnn_path.empty()) {
+            if (comb_type.compare(COMB_AVR) == 0) {
+                lm = new NGramRNNAverage(ngram_lm, rnn_lm);
+            } else if (comb_type.compare(COMB_MAX) == 0) {
+                lm = new NGramRNNMax(ngram_lm, rnn_lm);
+            } else {
+                std::cerr << "Error: " << comb_type << " is not a valid --comb_type." << std::endl;
+                usage(argv[0]);
+                return -1;
+            }
+        } else {
+            lm = ngram_lm;
+        }
     } else {
-        std::cerr << type << " is not a valid --type." << std::endl;
-        return -1;
+        lm = rnn_lm;
     }
 
     Benchmark *benchmark = new Benchmark(lm);
@@ -81,10 +114,19 @@ int main(int argc, char* argv[]) {
     benchmark_proto.set_perplexity(perplexity);
     benchmark_proto.set_average_keys_saved(average_keys_saved);
 
-    if (model_path.back() != '/') {
-        model_path += '/';
+    std::string benchmark_dir_path;
+    if (!save_path.empty()) {
+        benchmark_dir_path = save_path;
+    } else if (!ngram_path.empty()) {
+        benchmark_dir_path = ngram_path;
+    } else {
+        benchmark_dir_path = rnn_path;
     }
-    std::ofstream ofs (model_path + "benchmark.pbtxt", std::ios::out | std::ios::trunc);
+    if (benchmark_dir_path.back() != '/') {
+        benchmark_dir_path += '/';
+    }
+
+    std::ofstream ofs (benchmark_dir_path + "benchmark.pbtxt", std::ios::out | std::ios::trunc);
     google::protobuf::io::OstreamOutputStream osos(&ofs);
     if (!google::protobuf::TextFormat::Print(benchmark_proto, &osos)) {
         std::cerr << "Failed to write benchmark proto." << std::endl;
