a4716de933d5ed875f4ba78d52e3f8796242805d
Devan Kuleindiren <d.kuleindiren@gmail.com>
2017-02-25 21:46:04 +0000

    Fixed tokenise to keep alignment of original files.

--- util/tokenise.py
+++ util/tokenise.py
@@ -6,30 +6,21 @@ parser = argparse.ArgumentParser(description="Tokenise a given file.")
 parser.add_argument("--input_file_path", dest="input_file_path", help="The input file to tokenise.")
 parser.add_argument("--output_file_path", dest="output_file_path", help="The output file for the tokenised text.")
 parser.add_argument("--tweets", action="store_true" ,help="Set if the input file contains tweets.")
-parser.add_argument("--new_lines", action="store_true" ,help="Set if the input should be split into one sentence per line.")
 
 def main():
     args = parser.parse_args()
 
-    with open(args.input_file_path) as f:
-        # Read the file's text into a list of strings. If new_lines is set, then each string will be a sentence.
-        # Otherwise, there will just be one string representing the whole file.
-        if args.new_lines:
-            tokeniser = nltk.data.load('tokenizers/punkt/english.pickle')
-            text = tokeniser.tokenize(f.read().decode("utf-8"))
-        else:
-            text = [f.read().decode("utf-8")]
-
-    with open(args.output_file_path, "w") as f:
-        for s in text:
+    with open(args.input_file_path) as f_in, open(args.output_file_path, "w") as f_out:
+        for s in f_in:
+            s_encoded_lowercase = s.decode("utf-8").lower()
             if args.tweets:
-                line = nltk.tokenize.TweetTokenizer().tokenize(s)
+                line = nltk.tokenize.TweetTokenizer().tokenize(s_encoded_lowercase)
             else:
-                line = nltk.tokenize.word_tokenize(s)
+                line = nltk.tokenize.word_tokenize(s_encoded_lowercase)
             for w in line:
-                f.write(w.encode("utf-8"))
-                f.write(" ")
-            f.write("\n")
+                f_out.write(w.encode("utf-8"))
+                f_out.write(" ")
+            f_out.write("\n")
 
 if __name__ == "__main__":
     main()
